f460dc3ea19b4b4f1ae83bad704ad7a58ce4a794
peterson honorato <petersonphf@gmail.com>
2025-11-12 16:36:23 -0300

    terminando condiçoes e varios comandos em js

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 60.8
+
+console.log(`A velocidade do seu carro é ${vel}`)
+
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. Multado!`)
+}
+console.log(`Derija com segurança`)
