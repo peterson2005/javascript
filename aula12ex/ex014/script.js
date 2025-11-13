1249f7baa9a8d392a8c9c70519701a6c1061c225
peterson honorato <petersonphf@gmail.com>
2025-11-13 15:23:10 -0300

    primeiro projeto de js

---
+++ aula12ex/ex014/script.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+
+//var hora = 20
+
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 &&  hora < 12) {
+        img.src = 'imagens/manha.jpg'
+        document.body.style.background = '#d5e4a0ff'
+    } else if (hora >= 12 && hora < 18) {
+        img.src = 'imagens/tarde.jpg'
+        document.body.style.background = '#5f7475ff'
+    } else {
+        img.src = 'imagens/noite.jpg'
+        document.body.style.background = '#09263dff'
+    }
+}
